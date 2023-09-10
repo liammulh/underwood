0d5f14ed4ab995d086bf4866c33c18bfe002f324
Liam Mulhall <liammulh@gmail.com>
2023-09-10 13:31:40 -0700

    refactor: make `pretty_date` a private static method

--- src/underwood/page.py
+++ src/underwood/page.py
@@ -1,27 +1,15 @@
-"""Define a class that returns special pages in the blog.
+"""Define a class that returns the middle section of pages in the blog.
 
 As of this writing, the home page (index.html) and the archive page
-are treated specially. With other pages, we just grab the source and
-sandwich it between the top and bottom sections.
+are treated specially. With posts, we add some extra info and links.
+With other pages, we just grab the source and sandwich it between the
+top and bottom sections.
 """
 
 from string import Template
 from datetime import datetime as dt
 
 from src.underwood.config import Config
-
-
-def pretty_date(iso_8601_date: str) -> str:
-    """Return a date whose format is: Thursday, January 1, 1970.
-
-    Args:
-        iso_8601_date: date with format yyyy-mm-dd
-    """
-    date_obj = dt.strptime(iso_8601_date, "%Y-%m-%d")
-    weekday_and_month = date_obj.strftime("%A, %B ")
-    day = date_obj.strftime("%d").lstrip("0") + ", "
-    year = date_obj.strftime("%Y")
-    return weekday_and_month + day + year
 
 
 class Page:
@@ -34,7 +22,7 @@ class Page:
 
 
 class Home(Page):
-    """Define a class that gets the home section."""
+    """Define a class that gets the home page's middle section."""
 
     # fmt: off
     _post_summary_template = Template("""<h2>$post_title_link</h2>
@@ -44,8 +32,21 @@ $description $read_more_link
 </p>\n""")
     # fmt: on
 
+    @staticmethod
+    def _pretty_date(iso_8601_date: str) -> str:
+        """Return a date whose format is: Thursday, January 1, 1970.
+
+        Args:
+            iso_8601_date: date with format yyyy-mm-dd
+        """
+        date_obj = dt.strptime(iso_8601_date, "%Y-%m-%d")
+        weekday_and_month = date_obj.strftime("%A, %B ")
+        day = date_obj.strftime("%d").lstrip("0") + ", "
+        year = date_obj.strftime("%Y")
+        return weekday_and_month + day + year
+
     def get(self):
-        """Return the contents of the home page (index.html).
+        """Return the middle section of the home page (index.html).
 
         The home page contains a summary of the most recent posts from
         most to least recent. There is a global variable that can be
@@ -69,7 +70,7 @@ $description $read_more_link
                 )
                 home += self._post_summary_template.substitute(
                     post_title_link=post_title_link,
-                    pretty_date=pretty_date(post["published"]),
+                    pretty_date=self._pretty_date(post["published"]),
                     description=post["description"],
                     read_more_link=read_more_link,
                 )
