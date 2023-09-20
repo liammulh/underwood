f25683e4a3d8246759d5deae1dffa670fed7d03a
Liam Mulhall <liammulh@gmail.com>
2023-09-19 23:51:17 -0700

    chore: rename element tree variable

--- src/underwood/feed.py
+++ src/underwood/feed.py
@@ -1,6 +1,6 @@
 """Provides a class that is used to make an Atom feed."""
 
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
 from datetime import date
 
 from src.underwood.file import File
@@ -20,11 +20,11 @@ class Feed:
 
         # Create the root element of the XML document so that our
         # methods have it.
-        self.root = et.Element("feed", xmlns="http://www.w3.org/2005/Atom")
+        self.root = ET.Element("feed", xmlns="http://www.w3.org/2005/Atom")
 
     def metadata(self) -> None:
         """Write metadata to the root element in the XML document."""
-        et.SubElement(self.root, "title").text = self.info["domain_name"]
+        ET.SubElement(self.root, "title").text = self.info["domain_name"]
         # Borrow index.html's description for the subtitle. This assumes
         # index.html is the first page in the pages array. Not ideal,
         # but it's either this or duplicate the description in the JSON.
@@ -36,12 +36,12 @@ class Feed:
             if first_page["file"] == "index.html"
             else "Insert subtitle here"
         )
-        et.SubElement(self.root, "subtitle").text = subtitle
-        et.SubElement(
+        ET.SubElement(self.root, "subtitle").text = subtitle
+        ET.SubElement(
             self.root, "id"
         ).text = f"tag:{self.info['domain_name']},{self.info['inception_date']}:/"
-        et.SubElement(self.root, "updated").text = date.today().strftime("%Y-%m-%d")
-        et.SubElement(
+        ET.SubElement(self.root, "updated").text = date.today().strftime("%Y-%m-%d")
+        ET.SubElement(
             self.root,
             "link",
             rel="alternate",
@@ -55,28 +55,28 @@ class Feed:
         Args:
             post: blog post we're making an entry for
         """
-        entry = et.SubElement(self.root, "entry")
+        entry = ET.SubElement(self.root, "entry")
 
         # Write author element to the entry.
-        author = et.SubElement(entry, "author")
-        et.SubElement(author, "name").text = self.info["author"]
+        author = ET.SubElement(entry, "author")
+        ET.SubElement(author, "name").text = self.info["author"]
         site_url = post_url(self.info["domain_name"], "")
-        et.SubElement(author, "uri").text = site_url
+        ET.SubElement(author, "uri").text = site_url
 
         # Write post-specific elements to the entry.
-        et.SubElement(entry, "title").text = post["description"]
+        ET.SubElement(entry, "title").text = post["description"]
         url = post_url(self.info["domain_name"], post["file"])
-        et.SubElement(entry, "link").text = url
-        et.SubElement(entry, "id").text = uri(self.info, post)
+        ET.SubElement(entry, "link").text = url
+        ET.SubElement(entry, "id").text = uri(self.info, post)
         if "updated" in post:
             updated = post["updated"]
         else:
             updated = post["published"]
-        et.SubElement(entry, "updated").text = updated
-        et.SubElement(entry, "published").text = post["published"]
+        ET.SubElement(entry, "updated").text = updated
+        ET.SubElement(entry, "published").text = post["published"]
         for tag in post["tags"]:
-            et.SubElement(entry, "category", scheme=url, term=tag)
-        et.SubElement(entry, "summary", type="html").text = post["description"]
+            ET.SubElement(entry, "category", scheme=url, term=tag)
+        ET.SubElement(entry, "summary", type="html").text = post["description"]
 
     def entries(self):
         """Write all entries to the root of the XML document."""
@@ -89,5 +89,5 @@ class Feed:
         self.entries()
         xml_declaration = '<?xml version="1.0" encoding="utf-8"?>\n'
         output_file = File(f"{self.info['output_dir']}/feed.xml")
-        et.indent(self.root)
-        output_file.write(xml_declaration + et.tostring(self.root, encoding="unicode"))
+        ET.indent(self.root)
+        output_file.write(xml_declaration + ET.tostring(self.root, encoding="unicode"))
